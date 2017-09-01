ab94db6350271a9bb43695874dc75aac19f35c15
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-09-01 14:26:34 -0700

    redirect to home

--- newSite/src/App.js
+++ newSite/src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {Router, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import LandingPage from './views/landingPage'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <LandingPage />
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Route exact path='/' component={LandingPage} />
+            <Redirect to='/'/>
+          </Switch>
+        </div>
+      </Router>
     );
   }
 }
