e74fcbc5dee7ac5ffd33dc31a00a0f9095d1ac33
Rachel Ruderman <rachel@yembo.ai>
2019-03-12 23:31:32 -0700

    Update accreditations section

--- rachel-react/src/components/pages/About.js
+++ rachel-react/src/components/pages/About.js
@@ -3,34 +3,63 @@ import PageWrapper from "./PageWrapper";
 
 export default class About extends Component {
 
+    renderAccreditations = () => {
+        const accreditations = [
+            {
+                image:      'learn.jpg',
+                year:       '2017',
+                caption:    'Learn Academy JS Bootcamp',
+                url:        'https://www.youracclaim.com/badges/46c296df-8b13-4eb1-a30e-ec1ab6c80028'
+            },
+            {
+                image:      'pmp.png',
+                year:       '2017',
+                caption:    'Cert. # 2014353',
+                url:        'https://www.youracclaim.com/badges/46c296df-8b13-4eb1-a30e-ec1ab6c80028'
+            },
+            {
+                image:      'aws.png',
+                year:       '2018',
+                caption:    'Cert. # QD260LTKBMRQ1SC0',
+                url:        'https://aw.certmetrics.com/amazon/public/verification.aspx'
+            },
+        ];
+
+        return (
+            <div id='accreditations'>
+                {accreditations.map(accreditation => {
+                    const {image, year, caption, url} = accreditation;
+                    return (
+                        <div className='facts'>
+                            <a href={url} target='_blank' rel="noopener noreferrer">
+                                <img src={`img/certs/${image}`} alt={image}/>
+                                <p className="count">{year}</p>
+                                <p className="text-capitalize">{caption}</p>
+                            </a>
+                        </div>
+                    )
+
+                })}
+        </div>
+        )
+    };
     renderContent = () => {
         return (
-            <div>
-                <div>
-                    <h2 className="small-heading">TRANSLATOR => DEVELOPER</h2>
-                    <div className="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
-                        <div className="row">
-                            <div className="col-md-6 col-sm-12">
-                                <div className="video embed-responsive embed-responsive-4by3">
-                                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/t9sD-5tOx9A" allowFullScreen/>
-                                </div>
-                            </div>
+            <>
+                <h2 className="small-heading">Rachel Ruderman</h2>
+                {this.renderAccreditations()}
 
-                            <div className="col-md-6 col-sm-12">
-                                <div className="about-me-desc">
-                                    <p className='tagline'>
-                                        TRANSLATOR TURNED DEVELOPER
-                                    </p>
-                                    <div className='bio'>
-                                        <p>
-                                            After spending 5 years in Barcelona, Spanish and Catalan had become second nature to me. For a translator you might imagine this to be the ultimate success, but there was one noticeable drawback: the mental challenge was gone.
-                                        </p>
-                                        <p>
-                                            In 2017 I enrolled in LEARN Academy’s JavaScript Bootcamp and it's safe to say the mental challenge is back! While human languages take centuries to evolve, programming languages are constantly on the move – and that’s exactly what I love about coding.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
+                <div className="about-me-desc">
+                    <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/t9sD-5tOx9A" allowFullScreen/>
+                    <div>
+                        <p className='tagline'>TRANSLATOR TURNED DEVELOPER</p>
+                        <div className='bio'>
+                            <p>
+                                After spending 5 years in Barcelona, Spanish and Catalan had become second nature to me. For a translator you might imagine this to be the ultimate success, but there was one noticeable drawback: the mental challenge was gone.
+                            </p>
+                            <p>
+                                In 2017 I enrolled in LEARN Academy’s JavaScript Bootcamp and it's safe to say the mental challenge is back! While human languages take centuries to evolve, programming languages are constantly on the move – and that’s exactly what I love about coding.
+                            </p>
                         </div>
                     </div>
                 </div>
@@ -51,55 +80,12 @@ export default class About extends Component {
                         </div>
                     </div>
 
-                    <div className="clearfix">
-                        <h2 className="small-heading">EDUCATION</h2>
-                        <div className="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
-                            <div className="row">
-                                <div className="services col-md-4  col-xs-12">
-                                    <a href='https://www.learnacademy.org/' target='_blank'>
-                                        <div className="facts">
-                                            <div className="facts-overlay">
-                                                <i className="fa fa-code fa-3x"/>
-                                                <p className="count">2017</p>
-                                                <p className="text-capitalize">LEARN Academy JavaScript Bootcamp</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-
-                                <div className="services col-md-4  col-xs-12">
-                                    <a href='https://www.youracclaim.com/badges/46c296df-8b13-4eb1-a30e-ec1ab6c80028' target='_blank' rel="noopener noreferrer" >
-                                        <div className="facts">
-                                            <div className="facts-overlay">
-                                                <i className="fa fa-cogs fa-3x"/>
-                                                <p className="count">2017</p>
-                                                <p className="text-capitalize">Project Management Professional</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
 
 
-                                <div className="services col-md-4  col-xs-12">
-                                    <a href='http://lalis.richmond.edu/major-minor/index.html' target='_blank' rel="noopener noreferrer" >
-                                        <div className="facts">
-                                            <div className="facts-overlay">
-                                                <i className="fa fa-graduation-cap fa-3x"/>
-                                                <p className="count">2012</p>
-                                                <p>University of Richmond, BA</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                    </div>
-
-
-
-                </div>
-                    </div>
-            </div>
+            </>
         )
-    }
+    };
+
   render() {
     return (
         <PageWrapper image={'about2.gif'} title={'About Me'}>
--- rachel-react/src/components/pages/PageWrapper.js
+++ rachel-react/src/components/pages/PageWrapper.js
@@ -1,19 +1,23 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
 
-export default class PageWrapper extends Component {
+class PageWrapper extends Component {
     render() {
         const {children, title, image} = this.props;
+        const backgroundImage = `url(img/${image})`;
 
         return (
-            <div className='page-wrapper'>
+            <div className={`page-wrapper ${window.location.pathname.replace('/', '')}`}>
                 <div className='content'>{children}</div>
-                <div className='sidebar' style={{backgroundImage: `url(img/${image})`}}>
+                <div className='sidebar' style={{backgroundImage}}>
                     <div className="main-heading">
                         <h1>{title}</h1>
                     </div>
-                    <div className='close-btn'/>
+                    <div className='close-btn' onClick={() => this.props.history.push('/')}/>
                 </div>
             </div>
         );
     }
 }
+
+export default withRouter(PageWrapper);
