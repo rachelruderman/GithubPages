01a7709e7d964469724bae273af326268e6be1b6
Rachel Ruderman <raquel.rudermano@gmail.com>
2021-06-27 23:32:50 -0700

    Remove react

--- rachel-react/src/util/hidePreloader.js
+++
@@ -1,7 +0,0 @@
-export const hidePreloader = () => {
-    const preloader = document.getElementById('preloader');
-    if (preloader) {
-        preloader.style.opacity = 0;
-        preloader.style.zIndex = -1;
-    }
-};
